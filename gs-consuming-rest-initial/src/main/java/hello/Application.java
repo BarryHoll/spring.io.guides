4f086da625d284490b518a1b56cfac8c7bce5a90
BarryHoll <bchollgithub@gmail.com>
2015-05-17 20:28:39 +0100

    Initial commit after full implementation.

---
+++ gs-consuming-rest-initial/src/main/java/hello/Application.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.web.client.RestTemplate;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RestTemplate restTemplate = new RestTemplate();
+		Page page = restTemplate.getForObject("http://graph.facebook.com/pivotalsoftware", Page.class);
+		System.out.println("Name:    " + page.getName());
+		System.out.println("About:   " + page.getAbout());
+		System.out.println("Phone:   " + page.getPhone());
+		System.out.println("Website: " + page.getWebsite());
+	}
+
+}
